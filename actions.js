7216ecfa0ac5f244864d378c0241fbb8941fdfd4
SaraSaMatos <146724656+SaraSaMatos@users.noreply.github.com>
2023-10-03 12:27:34 +0100

    Set API icons

--- actions.js
+++ actions.js
@@ -90,6 +90,13 @@ function showTemperature(response) {
 
   let humidityInfo = document.querySelector("#humidityValue");
   humidityInfo.innerHTML = `${response.data.main.humidity} %`;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 navigator.geolocation.getCurrentPosition(showPosition);
 
